aceec6cb90354718e086819ffd9ced9972a28844
leaderbean <634051075@qq.com>
2018-08-30 23:55:28 +0800

    提交代码

---
+++ app/src/main/java/com/great/adou/app/App.java
@@ -0,0 +1,31 @@
+package com.great.adou.app;
+
+import android.app.Application;
+
+/**
+ *    <艺辉我爱你>
+ *
+ *     ***   ***
+ *   **    *    **
+ *     **     **
+ *        ***
+ *
+ *
+ * Created by WangWB on 2018/8/30:23:35.
+ * Email:634051075@qq.com
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+
+}
